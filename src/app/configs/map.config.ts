6cbe8d46b104e72a11ab0b463a1f07aea62932e9
klodian <dev.klodian@gmail.com>
2021-09-11 21:17:46 +0200

    const map configs

---
+++ src/app/configs/map.config.ts
@@ -0,0 +1,12 @@
+
+export const DefaultBounds: google.maps.LatLngBoundsLiteral = {
+  east: 30.061523437499996,
+  north: 46.04273709212578,
+  south: 34.849876732415346,
+  west: 10.813476562499996
+}
+export const DefaultCenter: google.maps.LatLngLiteral = {
+  lat: 40.446306912270565, lng: 20.437499999999996
+}
+
+export const DefaultRandom: {min: number , max: number} = {min: 9000 , max:15000}
